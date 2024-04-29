6bcc0f4d3a128e6cca798d6269996e929663b20e
Wandzfuture <michael.zabas@gmail.com>
2024-04-29 00:37:30 -0700

    web-flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that runs the app with a flask web framework
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Hello function"""
+    return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
