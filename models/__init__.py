0b644abfa2173e57971f59b1f0bbb46ebe45a0b2
Wandzfuture <michael.zabas@gmail.com>
2024-05-04 15:40:02 -0700

    0x02. AirBnB clone - MySQL

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,24 @@ If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
 instantiates a database storage engine (DBStorage).
 Otherwise, instantiates a file storage engine (FileStorage).
 """
-from os import getenv
+from os import environ
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+"""This checks the envirn var to determine storage method"""
+if environ['HBNB_TYPE_STORAGE'] == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
+
 else:
+    """The file storage selected"""
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import environ
 
 
 class State(BaseModel, Base):
     """Represents a state for a MySQL database."""
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
+            """
+            Returns a list of city instances with state_id that equals
+            to the current state.id
+            -> it will be the FileStorage relationship between
+            State & City
+            """
+            from models import storage
+            from models.city import City
+            """ Returns list of City objs in __objects """
+            cities_dict = storage.all(City)
+            cities_list = []
+
+            """ Copy values from dict to list """
+            for city in cities_dict.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    cities_list.append(city)
+
+            return cities_list
