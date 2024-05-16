64c0d7932ef7d7bef7a03493d42e0cbc8067ddf1
Wandzfuture <michael.zabas@gmail.com>
2024-05-16 16:45:52 -0700

    0x03. AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-""" Web application deployement module """
+"""
+a Fabric script that deletes out-of-date archives
+execute: fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+import os
 from fabric.api import *
 
-
-env.hosts = ['35.231.33.237', '34.74.155.163']
-env.user = "ubuntu"
+env.hosts = ['18.234.253.252', '52.91.153.152']
 
 
 def do_clean(number=0):
-    """ deletes the out-of-date archives of the static file """
+    """
+    Deletes out-of-date archives.
 
-    number = int(number)
+    Args:
+        number (int): Number of archives to keep. If 0 or 1, keeps only the most recent.
+                      If 2, keeps the two most recent, and so on.
+    """
+    number = 1 if int(number) == 0 else int(number)
 
-    if number == 0:
-        number = 2
-    else:
-        number += 1
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
