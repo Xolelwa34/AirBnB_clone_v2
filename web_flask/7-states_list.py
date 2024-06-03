e6ed1c886bd959c63df119e4fd706dbab6f4b9cf
Wandzfuture <michael.zabas@gmail.com>
2024-06-03 07:31:50 +0000

    0x04. AirBnB clone - Web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
 """
-This module contains a script that starts a Flask web application.
-Imports necessary modules, sets up routes,& handles application teardown.
+Start Flask application
 """
 
 from flask import Flask, render_template
 from models import *
 from models import storage
-
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    This function handles the '/states_list' route.
-    It fetches all the states, sorts them in alphabetical order,
-    and renders them on a HTML page.
-    """
+    """display a HTML page with the states listed in alphabetical order"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-    This function is called when the application context tears down.
-    It closes the storage.
-    """
+    """closes the storage on teardown"""
     storage.close()
 
 if __name__ == '__main__':
-    """
-    This block ensures the Flask application only runs if the script
-    is executed directly. When the script is imported, nothing happens.
-    """
     app.run(host='0.0.0.0', port='5000')
