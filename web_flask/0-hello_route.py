c40429d467b8172acf98c65d6db237abe2d1590e
Xolelwa34 <ciliaswekile@gmail.com>
2024-08-22 11:33:18 +0000

    adding onepage

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -16,5 +16,16 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
+
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def airbnb_onepage():
+    """
+    Displays a welcome message for the Airbnb one page.
+
+    Returns:
+        str: The text "Welcome to the Airbnb One Page!".
+    """
+    return "Hello HBNB!"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
